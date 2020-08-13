97d5a06768e7249087232f789bdc0b2368c9b999
Garrett Brown <gman9mm@live.com>
2020-08-13 12:08:38 -0700

    hide images, eliminates duplicate question.
    co-authored-by: Natalie Murphy <nataliemurphy500@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,21 @@
 $(document).ready(function() {
   $(".btn").click(function() {
-    $(".dating_quiz").show();
+    $("#dating_quiz").show();
+  });
+
+  $("#dating_quiz").submit(function(event) {
+       
+    const beverage = $("#beverage").val();
+
+    if ("1") :upload.wikimedia.org/wikipedia/commons/9/99/Chris_Pratt_2018.jpg)
+    if ("2") :static.scientificamerican.com/blogs/cache/file/7069F0BB-A9AB-4932-84F508BBC0136458_source.jpg?w=590&h=800&F754D658-CE37-41EE-BE2C0EFC7134D582)
+    if ("3") :upload.wikimedia.org/wikipedia/commons/1/1e/Randy_Savage_1986.jpg)
+    if ("4") :pmcvariety.files.wordpress.com/2019/08/shutterstock_editorial_10369663y.jpg?w=681&h=383&crop=1)
+    if ("5") :filmschoolrejects.com/wp-content/uploads/2019/08/jeff-goldblum.jpg)
+
+    const person1Input = $("input#name").val();
+    $(".name").text(nameInput);
+    event.preventDefault();
   });
 });
 
-    
-  const beverage = $("#beverage").val();
-
-  if (option==="1" :upload.wikimedia.org/wikipedia/commons/9/99/Chris_Pratt_2018.jpg)
-  if (option==="2" :static.scientificamerican.com/blogs/cache/file/7069F0BB-A9AB-4932-84F508BBC0136458_source.jpg?w=590&h=800&F754D658-CE37-41EE-BE2C0EFC7134D582)
-  if (option==="3" :upload.wikimedia.org/wikipedia/commons/1/1e/Randy_Savage_1986.jpg)
-  if (option==="4" :pmcvariety.files.wordpress.com/2019/08/shutterstock_editorial_10369663y.jpg?w=681&h=383&crop=1)
-  if (option==="5" :filmschoolrejects.com/wp-content/uploads/2019/08/jeff-goldblum.jpg)
-
-  event.preventDefault();
