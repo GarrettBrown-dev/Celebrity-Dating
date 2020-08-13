690cb07796fa2de7d9d5e3caf741d5d17582d450
Garrett Brown <gman9mm@live.com>
2020-08-13 11:35:10 -0700

    Add new things.
    co-authored-by: Natalie Murphy <nataliemurphy500@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -7,10 +7,10 @@ $(document).ready(function() {
     
   const beverage = $("#beverage").val();
 
-  if (option==="1" display:https://upload.wikimedia.org/wikipedia/commons/9/99/Chris_Pratt_2018.jpg)
-  if (option==="2" display:https://static.scientificamerican.com/blogs/cache/file/7069F0BB-A9AB-4932-84F508BBC0136458_source.jpg?w=590&h=800&F754D658-CE37-41EE-BE2C0EFC7134D582)
-  if (option==="3" display:https://upload.wikimedia.org/wikipedia/commons/1/1e/Randy_Savage_1986.jpg)
-  if (option==="4" display:https://pmcvariety.files.wordpress.com/2019/08/shutterstock_editorial_10369663y.jpg?w=681&h=383&crop=1  )
-  if (option==="5" display:https://filmschoolrejects.com/wp-content/uploads/2019/08/jeff-goldblum.jpg)
+  if (option==="1" :upload.wikimedia.org/wikipedia/commons/9/99/Chris_Pratt_2018.jpg)
+  if (option==="2" :static.scientificamerican.com/blogs/cache/file/7069F0BB-A9AB-4932-84F508BBC0136458_source.jpg?w=590&h=800&F754D658-CE37-41EE-BE2C0EFC7134D582)
+  if (option==="3" :upload.wikimedia.org/wikipedia/commons/1/1e/Randy_Savage_1986.jpg)
+  if (option==="4" :pmcvariety.files.wordpress.com/2019/08/shutterstock_editorial_10369663y.jpg?w=681&h=383&crop=1)
+  if (option==="5" :filmschoolrejects.com/wp-content/uploads/2019/08/jeff-goldblum.jpg)
 
   event.preventDefault();
